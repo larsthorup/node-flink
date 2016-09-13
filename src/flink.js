28551bd60ac160ab2b2ce6b1bd714593ac65092a
Lars Thorup <lars@zealake.com>
2016-09-13 13:16:58 +0200

    now with SumOperator

--- src/flink.js
+++ src/flink.js
@@ -66,6 +66,28 @@ class KeyByOperator {
   }
 }
 
+class SumOperator {
+  constructor (index, source) {
+    this.source = source;
+    this.stream = new Stream();
+    this.index = index;
+    this.state = {};
+  }
+  start () {
+    this.source.stream.on('data', (value) => {
+      const key = value.key;
+      const val = value.value[this.index];
+      const oldValue = this.state.hasOwnProperty(key) ? this.state[key] : 0;
+      const newValue = oldValue + val;
+      this.state[key] = newValue;
+      const keyedValue = {key: key, value: newValue};
+      this.stream.trigger('data', keyedValue);
+    });
+    this.source.stream.on('end', () => { this.stream.trigger('end'); })
+    this.source.start();
+  }
+}
+
 class PrintSink {
   constructor (source) {
     this.source = source;
@@ -90,13 +112,12 @@ class DataStream {
     this.env.plan = new KeyByOperator(index, this.env.plan);
     return this;
   }
-  sum () {
-    // ToDo: add sum to plan
+  sum (index) {
+    this.env.plan = new SumOperator(index, this.env.plan)
     return this;
   }
   print () {
     this.env.plan = new PrintSink(this.env.plan);
-    // ToDo: add print to plan
   }
 }
 
